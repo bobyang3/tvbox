12fc14ccd3ffc9c8b42ad659f30ad89189084f6a
Bob Yang <bobyang3@gmail.com>
2025-12-05 10:43:45 -0800

    feat: Add TVBox OSC service with channel data, PHP script, and configuration files.

--- TVBoxOSC/srve00/index.php
+++ TVBoxOSC/srve00/index.php
@@ -1,10 +1,9 @@
 
 <?php
-//header("Location: https://rb.gy/fkbt61");
- //header("Location: https://robyang.serv00.net/VodLive/sb8ffa.php");
+//header("Location: https://robyang.serv00.net/VodLive/sb8ffa.php");
 //     header("Location: https://raw.githubusercontent.com/bobyang3/tvbox/refs/heads/own/source.json");
-header("Location: http://www1.robyang.dpdns.org/VodLive/sb8ffa");
-
+//header("Location: http://www1.robyang.dpdns.org/VodLive/sb8ffa");
+header("Location: http://robyang.dpdns.org");
 
 
     exit;
