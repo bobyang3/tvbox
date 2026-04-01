ec506401a45c3a4d80ea687adf9e9971aa89b7b4
Bob Yang <2717137+bobyang3@users.noreply.github.com>
2026-04-01 13:59:18 -0700

    update

---
+++ TVBoxOSC/apks/rename.bat
@@ -0,0 +1,9 @@
+cd /d "%~dp0"
+ren "leanback-pro-v8a.apk" "OKTVPro-TV64.apk"
+ren "leanback-pro.apk" "OKTVPro-TV32.apk"
+ren "mobile-pro.apk" "OKTVPro-mobile.apk"
+
+ren "mobile-armeabi_v7a.apk" "OKTV-mobile.apk"
+ren "mobile-arm64_v8a.apk" "OKTV-mobile64.apk"
+ren "leanback-armeabi_v7a.apk" "OKTV-TV.apk"
+ren "leanback-arm64_v8a.apk" "OKTV-TV64.apk"
