bf18c44546301f57af963da5cbc9ee418521d383
Bob Yang <2717137+bobyang3@users.noreply.github.com>
2026-05-11 08:26:42 -0700

    feat: add 4KAV source, update TVBoxOSC sources, and provide APK renaming script

---
+++ apks/rename.bat
@@ -0,0 +1,35 @@
+:: PRO 5.1.7
+:: OK 5.0.10
+cd /d "%~dp0"
+ren "leanback-pro-v8a.apk" "OKTVPro-TV64.apk"
+ren "leanback-pro.apk" "OKTVPro-TV32.apk"
+ren "mobile-pro.apk" "OKTVPro-mobile.apk"
+
+ren "mobile-armeabi_v7a.apk" "OKTV-mobile.apk"
+ren "mobile-arm64_v8a.apk" "OKTV-mobile64.apk"
+ren "leanback-armeabi_v7a.apk" "OKTV-TV.apk"
+ren "leanback-arm64_v8a.apk" "OKTV-TV64.apk"
+
+
+:: http://aftv.news/1389234
+:: https://github.com/lystv/fmapp/releases/download/5.2.1-pro/leanback-pro-v8a.apk
+
+:: http://aftv.news/9454479
+:: https://github.com/lystv/fmapp/releases/download/5.2.1-pro/leanback-pro.apk
+
+:: http://aftv.news/2067745
+:: https://github.com/lystv/fmapp/releases/download/5.2.1-pro/mobile-pro.apk
+
+:: http://aftv.news/8396282
+:: http://www3.robyang.qzz.io/apks/OKTV-mobile.apk
+
+:: http://aftv.news/3179144
+:: http://www3.robyang.qzz.io/apks/OKTV-mobile64.apk
+
+:: http://aftv.news/6931895
+:: http://www3.robyang.qzz.io/apks/OKTV-TV.apk
+
+:: http://aftv.news/2695416
+:: http://www3.robyang.qzz.io/apks/OKTV-TV64.apk
+
+pause
